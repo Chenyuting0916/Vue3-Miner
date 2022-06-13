6255a99c0c5192cdd3c1a1c9fa79edcc51c299f4
tim.chen73 <tim.chen@titansoft.com.sg>
2022-06-13 22:20:01 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>{{ Name }}</div>
+  <div>{{ price1 + price2 }}</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      Name: "hi",
+      price1: 5,
+      price2: 63
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
